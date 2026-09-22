8dc15596ea75b5ec52b027f498ff6dc431cc8e47
jiyoon-lee <ktcloudljy@gmail.com>
2026-09-22 20:13:26 +0900

    feat: RabbitMQ WMS OMS credential 분리

--- init/secrets.tf
+++ init/secrets.tf
@@ -1,11 +1,39 @@
 # ==============================================================================
-# RabbitMQ Application credential container
+# RabbitMQ Application credential containers
 # ==============================================================================
 
 # SecretVersion은 Terraform에서 관리하지 않습니다.
 resource "aws_secretsmanager_secret" "rabbitmq_app_credentials" {
   name                    = "prod/total/rabbitmq-app-credentials"
   description             = "RabbitMQ total-backend application credentials for total-prod"
+  kms_key_id              = data.aws_kms_alias.secrets_cmk.target_key_arn
+  recovery_window_in_days = 7
+
+  tags = {
+    Environment = "prod"
+    ManagedBy   = "terraform"
+    Compliance  = "ISMS-P-2.7.2"
+  }
+}
+
+# SecretVersion은 Terraform에서 관리하지 않습니다.
+resource "aws_secretsmanager_secret" "rabbitmq_wms_credentials" {
+  name                    = "prod/total/rabbitmq-wms-credentials"
+  description             = "RabbitMQ total-wms application credentials for total-prod"
+  kms_key_id              = data.aws_kms_alias.secrets_cmk.target_key_arn
+  recovery_window_in_days = 7
+
+  tags = {
+    Environment = "prod"
+    ManagedBy   = "terraform"
+    Compliance  = "ISMS-P-2.7.2"
+  }
+}
+
+# SecretVersion은 Terraform에서 관리하지 않습니다.
+resource "aws_secretsmanager_secret" "rabbitmq_oms_credentials" {
+  name                    = "prod/total/rabbitmq-oms-credentials"
+  description             = "RabbitMQ total-oms application credentials for total-prod"
   kms_key_id              = data.aws_kms_alias.secrets_cmk.target_key_arn
   recovery_window_in_days = 7
 
